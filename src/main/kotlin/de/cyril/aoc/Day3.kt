6b6cb8e76c02dd04e6d9b74defca7127f8dbe1f8
cyril <kpovolotsk@zoho.com>
2020-12-13 16:22:51 +0100

    Day3 2

--- src/main/kotlin/de/cyril/aoc/Day3.kt
+++ src/main/kotlin/de/cyril/aoc/Day3.kt
@@ -1,22 +1,36 @@
 package de.cyril.aoc
 
+private val lines: List<List<Boolean>> = InputReader.readLines("Day3.txt")
+    .map { line ->
+        line.map { it == '#' }
+    }
+
+private val columnCount = lines[0].size
+
 
 fun main() {
-    val lines: List<List<Boolean>> = InputReader.readLines("Day3.txt")
-        .map { line ->
-            line.map { it == '#' }
-        }
-    val columnCount = lines[0].size
+    val result = countTrees(1, 1) *
+            countTrees(3, 1) *
+            countTrees(5, 1) *
+            countTrees(7, 1) *
+            countTrees(1, 2)
 
+    println(result)
+}
+
+private fun countTrees(right: Int, down: Int): Long {
     var columnIndex = 0;
     var lineIndex = 0;
 
     val treeCount = (1 until lines.size).count {
-        columnIndex = (columnIndex + 3) % columnCount
-        lineIndex++
-        lines[lineIndex][columnIndex]
+        columnIndex = (columnIndex + right) % columnCount
+        lineIndex += down
+
+        if (lineIndex > lines.size - 1) {
+            false
+        } else {
+            lines[lineIndex][columnIndex]
+        }
     }
-
-    println(treeCount)
-
+    return treeCount.toLong()
 }
