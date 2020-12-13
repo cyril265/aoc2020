5bcf96a332703cf10567b6e9f1a3f53a3f90387d
cyril <kpovolotsk@zoho.com>
2020-12-13 15:20:44 +0100

    Day1

---
+++ src/main/kotlin/de/cyril/aoc/Day1.kt
@@ -0,0 +1,25 @@
+package de.cyril.aoc
+
+
+val input = ClassLoader.getSystemResource("Day1.txt").readText()
+    .split("\r\n")
+    .map { it.toInt() }
+
+fun main() {
+    val result = input.asSequence()
+        .mapNotNull { firstNumber ->
+            sumIfMatches(firstNumber)
+        }
+        .first()
+    println(result)
+}
+
+fun sumIfMatches(firstNumber: Int): Int? {
+    val matchingSecondNumber = input.filter { it != firstNumber }
+        .find { secondNumber -> secondNumber + firstNumber == 2020 }
+
+    return if (matchingSecondNumber != null) {
+        matchingSecondNumber * firstNumber;
+    } else
+        null
+}
