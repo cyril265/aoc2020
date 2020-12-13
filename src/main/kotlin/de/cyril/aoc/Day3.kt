4552983a2297e026b541f09af3936927baac8605
cyril <kpovolotsk@zoho.com>
2020-12-13 16:11:07 +0100

    Day3

---
+++ src/main/kotlin/de/cyril/aoc/Day3.kt
@@ -0,0 +1,22 @@
+package de.cyril.aoc
+
+
+fun main() {
+    val lines: List<List<Boolean>> = InputReader.readLines("Day3.txt")
+        .map { line ->
+            line.map { it == '#' }
+        }
+    val columnCount = lines[0].size
+
+    var columnIndex = 0;
+    var lineIndex = 0;
+
+    val treeCount = (1 until lines.size).count {
+        columnIndex = (columnIndex + 3) % columnCount
+        lineIndex += 1
+        lines[lineIndex][columnIndex]
+    }
+
+    println(treeCount)
+
+}
